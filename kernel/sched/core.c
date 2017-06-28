331adf84050b683ca158bbc578b81b88eda1b462
Youngtae Lee <yt0729.lee@samsung.com>
2017-06-28 03:35:54 -0700

    [9820] sched: core: fix bug that rt could not be binded to little
    Current rt little binding code is working
    when target task is on runqueue. Remove this condition
    to all rt tasks bind to little.
    
    Change-Id: I2f1883e2e8dffef25920848f6ef87b38a231ff0c
    Signed-off-by: Youngtae Lee <yt0729.lee@samsung.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -4155,6 +4155,15 @@ change:
 		enqueue_task(rq, p, queue_flags);
 	}
 
+	if (p->sched_class == &rt_sched_class) {
+		struct cpumask mask;
+
+		cpumask_andnot(&mask, &p->cpus_allowed, cpu_coregroup_mask(4));
+		cpumask_andnot(&mask, &p->cpus_allowed, cpu_coregroup_mask(6));
+		if (!cpumask_empty(&mask))
+			do_set_cpus_allowed(p, &mask);
+	}
+
 	check_class_changed(rq, p, prev_class, oldprio);
 	preempt_disable(); /* avoid rq from going away on us */
 	task_rq_unlock(rq, p, &flags);
