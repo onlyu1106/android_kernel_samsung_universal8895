234b6e78764be92020d1979e18095e5095e8cad8
lakkyung.jung <lakkyung.jung@samsung.com>
2018-03-14 13:18:58 +0900

    [COMMON] cpuidle: Add to get target residency for cpu.
    Change-Id: I9c59acd8bdda34d7071d8512c8551dcb76eee5b5
    Signed-off-by: lakkyung.jung <lakkyung.jung@samsung.com>

--- include/linux/cpuidle.h
+++ include/linux/cpuidle.h
@@ -158,6 +158,7 @@ extern void disable_priv_cpuidle(void);
 extern void enable_priv_cpuidle(void);
 
 extern struct cpuidle_driver *cpuidle_get_cpu_driver(struct cpuidle_device *dev);
+extern unsigned int cpuidle_get_target_residency(int cpu, int state);
 #else
 static inline void disable_cpuidle(void) { }
 static inline bool cpuidle_not_available(struct cpuidle_driver *drv,
@@ -193,6 +194,7 @@ static inline void cpuidle_disable_device(struct cpuidle_device *dev) { }
 static inline int cpuidle_play_dead(void) {return -ENODEV; }
 static inline struct cpuidle_driver *cpuidle_get_cpu_driver(
 	struct cpuidle_device *dev) {return NULL; }
+static inline unsigned int cpuidle_get_target_residency(int cpu, int state) {return UINT_MAX;}
 #endif
 
 #if defined(CONFIG_CPU_IDLE) && defined(CONFIG_SUSPEND)
