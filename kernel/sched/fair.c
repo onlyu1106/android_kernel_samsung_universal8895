f2b2d1575674139b3a8d768c53c904cf4bf5b622
Soohyun Kim <soohyuni.kim@samsung.com>
2018-03-28 16:15:33 +0900

    [COMMON] sched/rt : fix unnecessary operation for PELT
    Change-Id: I3579565f8025b151b303862a710374963197731e
    Signed-off-by: Soohyun Kim <soohyuni.kim@samsung.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8117,7 +8117,7 @@ static void update_blocked_averages(int cpu)
 		if (cfs_rq->tg->se[cpu])
 			update_load_avg(cfs_rq->tg->se[cpu], 0);
 	}
-	update_rt_rq_load_avg(rq_clock_task(rq), cpu, &rq->rt, 0);
+
 #ifdef CONFIG_NO_HZ_COMMON
 	rq->last_blocked_load_update_tick = jiffies;
 #endif
@@ -8180,7 +8180,6 @@ static inline void update_blocked_averages(int cpu)
 	raw_spin_lock_irqsave(&rq->lock, flags);
 	update_rq_clock(rq);
 	update_cfs_rq_load_avg(cfs_rq_clock_task(cfs_rq), cfs_rq, true);
-	update_rt_rq_load_avg(rq_clock_task(rq), cpu, &rq->rt, 0);
 #ifdef CONFIG_NO_HZ_COMMON
 	rq->last_blocked_load_update_tick = jiffies;
 #endif
