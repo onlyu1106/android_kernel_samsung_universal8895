661c62c9b2ce52002185282b298c0fdac2d41d8e
Diep Quynh <remilia.1505@gmail.com>
2021-01-10 21:46:39 +0700

    drivers: Further reduce periodic log spam
    Signed-off-by: Diep Quynh <remilia.1505@gmail.com>

--- drivers/battery_v2/sec_battery.c
+++ drivers/battery_v2/sec_battery.c
@@ -6727,7 +6727,7 @@ static int sec_bat_get_property(struct power_supply *psy,
 		psy_do_property(battery->pdata->fuelgauge_name, get,
 				POWER_SUPPLY_PROP_VOLTAGE_NOW, value);
 		battery->voltage_now = value.intval;
-		dev_err(battery->dev,
+		dev_dbg(battery->dev,
 			"%s: voltage now(%d)\n", __func__, battery->voltage_now);
 		/* voltage value should be in uV */
 		val->intval = battery->voltage_now * 1000;
@@ -6737,7 +6737,7 @@ static int sec_bat_get_property(struct power_supply *psy,
 		psy_do_property(battery->pdata->fuelgauge_name, get,
 				POWER_SUPPLY_PROP_VOLTAGE_AVG, value);
 		battery->voltage_avg = value.intval;
-		dev_err(battery->dev,
+		dev_dbg(battery->dev,
 			"%s: voltage avg(%d)\n", __func__, battery->voltage_avg);
 		/* voltage value should be in uV */
 		val->intval = battery->voltage_avg * 1000;
